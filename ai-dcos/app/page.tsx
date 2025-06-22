f722a9114c1b0fed7c549b77280a7815b0991074
senpaiharde <xxslavan1@gmail.com>
2025-06-22 18:33:21 +0300

    added styles to the img

--- ai-dcos/app/page.tsx
+++ ai-dcos/app/page.tsx
@@ -72,16 +72,19 @@ export default function Home() {
             <Link href="/dashboard">Get Started</Link>
           </Button>
         </div>
-        <div>
-         
-          <div>
+        <div className="relative overflow-hidden pt-16">
+          <div className="mx-auto max-w-7xl px-6 lg:px-8">
             <Image
-          alt='App screenshot'
-          src='https://i.imgur.com/VciRSTI.jpeg'
-          width={2432}
-          height={1442}
-          className='mb-[-0%] rounded-xl shadow-2xl right-1 ring-gray-900/10'
-          /></div>
+              alt="App screenshot"
+              src="https://i.imgur.com/VciRSTI.jpeg"
+              width={2432}
+              height={1442}
+              className="mb-[-0%] rounded-xl shadow-2xl right-1 ring-gray-900/10"
+            />
+            <div aria-hidden="true" className="relative">
+              <div className="absolute bottom-0 -inset-x-32 bg-gradient-to-t from-white/95 pt-[5%]" />
+            </div>
+          </div>
         </div>
       </div>
     </main>
