59f977d36ec31fefc5461b2b9de71a61292f194e
senpaiharde <xxslavan1@gmail.com>
2025-06-22 18:26:08 +0300

    chore: ignore Next.js build output (.next/)

--- ai-dcos/.next/static/chunks/app_layout_tsx_c0237562._.js
+++
@@ -1,9 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules_@clerk_nextjs_dist_esm_app-router_d0fe68e3._.js",
-  "static/chunks/node_modules_dd580c9c._.js",
-  "static/chunks/app_globals_73c37791.css"
-],
-    source: "dynamic"
-});
