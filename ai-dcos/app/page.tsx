0815c97947835f6cd9c28ee48958ed39f5462b74
senpaiharde <xxslavan1@gmail.com>
2025-06-22 18:53:04 +0300

    added more styles to desgin to each section on the run over feattures now icons are on relatvie + the desgin more clear and up line with each line 3 ob

--- ai-dcos/app/page.tsx
+++ ai-dcos/app/page.tsx
@@ -88,11 +88,15 @@ export default function Home() {
         </div>
 
         <div className=''>
-            <dl>
+            <dl className='mx-auto grid max-w-2xl grid-cols-1 gap-x-6 
+            gap-y-10 text-base leading-7 text-gray-600 sm:grid-cols-2 
+            lg:mx-0 lg:max-w-none lg:grid-cols-3 lg:gap-x-8 lg:gap-y-16'>
                 {features.map((feature) => (
-                    <div key={feature.name}>
-                        <dt className=' inline font-semibold'>
-                            <feature.icon />
+                    <div className='relative pl-9' key={feature.name}>
+                        <dt className=' inline font-semibold text-gray-900'>
+                            <feature.icon 
+                            aria-hidden="true"
+                            className='absolute left-1 top-1 h-5 w-5'/>
                         </dt>
                         <dd>{feature.description}</dd>
                     </div>
