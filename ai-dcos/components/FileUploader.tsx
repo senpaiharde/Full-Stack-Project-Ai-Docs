1ece55981e428c8c10aa0c128c10769de1d454d6
senpaiharde <xxslavan1@gmail.com>
2025-06-23 18:16:15 +0300

    added emjis with styles for better overview

--- ai-dcos/components/FileUploader.tsx
+++ ai-dcos/components/FileUploader.tsx
@@ -1,4 +1,5 @@
 'use client';
+import { CircleArrowDown, RocketIcon } from 'lucide-react';
 import React, { useCallback } from 'react';
 import { useDropzone } from 'react-dropzone';
 function FileUploader() {
@@ -17,11 +18,17 @@ function FileUploader() {
           isFocused || isDragAccept ? 'border-indigo-300' : 'border-indigo-100'
         }`}>
         <input {...getInputProps()} />
-        <div className="">
+        <div className="flex flex-col items-center justify-center">
           {isDragActive ? (
-            <p>Drop the files here ...</p>
+            <>
+              <RocketIcon className="h-20 w-20 animate-ping" />
+              <p>Drop the files here ...</p>
+            </>
           ) : (
-            <p>Drag 'n' drop some files here, or click to select files</p>
+            <>
+            <CircleArrowDown className='h-20 w-20 animate-bounce'/>
+              <p>Drag 'n' drop some files here, or click to select files</p>
+            </>
           )}
         </div>
       </div>
