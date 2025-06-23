67a30b7f6103f61cb999a7b77b96d8efb3dc3ccc
senpaiharde <xxslavan1@gmail.com>
2025-06-23 17:52:14 +0300

    added the my doucuments upload file area with desgin

--- ai-dcos/app/dashboard/page.tsx
+++ ai-dcos/app/dashboard/page.tsx
@@ -1,9 +1,13 @@
-import React from 'react'
-
+import Documents from '@/components/ui/Documents';
+import React from 'react';
+export const dynamic = 'force-dynamic'
 function dashboard() {
   return (
-    <div>dashboard</div>
-  )
+    <div className="h-full max-w-7xl mx-auto">
+      <h1 className="text-3xl p-5 bg-gray-100 font-light text-indigo-600">My Documents</h1>
+     <Documents />
+    </div>
+  );
 }
 
-export default dashboard
+export default dashboard;
