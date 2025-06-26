f3d704dbd6d7dc56915c5337f8aea2a4f0538294
senpaiharde <xxslavan1@gmail.com>
2025-06-26 20:15:32 +0300

    added styles + create pdfview file will be on left

--- ai-dcos/app/dashboard/files/[id]/page.tsx
+++ ai-dcos/app/dashboard/files/[id]/page.tsx
@@ -30,8 +30,8 @@ async function ChatToFilePage({
   const respone = await fetch(url);
   return (
     <div className="grid lg:grid-cols-5 h-full overflow-hidden">
-      <div></div>
-      <div></div>
+      <div className="lg-col-span-2 overflow-y-auto"></div>
+      <div className="col-span-5 lg:col-span-3 bg-gray-100 border-r-2 lg:border-indigo-600 lg:-order-1 overflow-auto"></div>
     </div>
   );
 }
---
+++ ai-dcos/components/PdfView.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function PdfView({url} : {url:string}) {
+  return (
+    <div>PdfView</div>
+  )
+}
+
+export default PdfView
