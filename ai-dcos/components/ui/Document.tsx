b501a2c6bf075e39a674cbe1295855ba643a5c41
senpaiharde <xxslavan1@gmail.com>
2025-06-28 17:51:42 +0300

    added name/ bytes and link to doc

--- ai-dcos/components/ui/Document.tsx
+++ ai-dcos/components/ui/Document.tsx
@@ -1,8 +1,8 @@
-"use client";
+'use client';
 
 import { useRouter } from 'next/navigation';
-import { useTransition } from "react";
-
+import { useTransition } from 'react';
+import byteSize from "byte-size";
 function Document({
   id,
   name,
@@ -17,8 +17,18 @@ function Document({
   const router = useRouter();
   const [isDeleting, startTransaction] = useTransition();
   //const { hasActiveMembership } = useSubscription();
-  return <div className='flex flex-col w-64 h-80 rounded-xl bg-white drop-shadow-md 
-  justify-between p-4 transition-all transform hover:scale-105 hover:bg-indigo-600 hover:text-white cursor-pointer group'>Document</div>;
+  return (
+    <div
+      className="flex flex-col w-64 h-80 rounded-xl bg-white drop-shadow-md 
+  justify-between p-4 transition-all transform hover:scale-105 hover:bg-indigo-600 hover:text-white cursor-pointer group">
+      <div 
+      className='flex-1'
+      onClick={() => router.push(`/dashboard/files/${id}`)}>
+        <p className='font-semibold line-clamp-2'>{name}</p>
+        <p className='text-sm text-gray-500 group-hover:text-indigo-100'>{byteSize(size).value}KB</p>
+      </div>
+    </div>
+  );
 }
 
 export default Document;
