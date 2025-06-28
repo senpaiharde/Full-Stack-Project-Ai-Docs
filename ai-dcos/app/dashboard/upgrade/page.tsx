ccccb602a37fc01ad84edf3e151dc81957d2328a
senpaiharde <xxslavan1@gmail.com>
2025-06-28 18:47:24 +0300

    more text

--- ai-dcos/app/dashboard/upgrade/page.tsx
+++ ai-dcos/app/dashboard/upgrade/page.tsx
@@ -1,3 +1,4 @@
+import { CheckIcon } from 'lucide-react';
 import React from 'react';
 
 function PricingPage() {
@@ -15,18 +16,31 @@ function PricingPage() {
           enhancing productivity, and streamlining your workflow.
         </p>
         <div>
-            <div>
-                <h3 className='text-lg font-semibold leading-8 text-gray-900'>
-                    Starter Plan
-                </h3>
-                <p className='mt-4 text-sm leading-6 text-gray-600'>
-                    Explore core Features at No Cost
-                </p>
-                
-            </div>
+          <div>
+            <h3 className="text-lg font-semibold leading-8 text-gray-900">Starter Plan</h3>
+            <p className="mt-4 text-sm leading-6 text-gray-600">Explore core Features at No Cost</p>
+            <p className='mt-6 flex items-baseline gap-x-1'>
+              <span className="text-4xl font-bold tracking-tight text-gray-900">Free</span>
+            </p>
+            <ul 
+            role='list'
+            className='mt-8 space-y-3 text-sm loading-6 text-gray-600'>
+                <li className='flex gap-x-3'>
+                    <CheckIcon className='h-6 w-5 flex-none text-indigo-600'/>
+                    Documents
+                </li>
+                 <li className='flex gap-x-3'>
+                    <CheckIcon className='h-6 w-5 flex-none text-indigo-600'/>
+                    Up to 3 messages per documents
+                </li>
+                 <li className='flex gap-x-3'>
+                    <CheckIcon className='h-6 w-5 flex-none text-indigo-600'/>
+                    Try out the AI chat Functionality
+                </li>
+            </ul>
+          </div>
 
-
-            <div></div>
+          <div></div>
         </div>
       </div>
     </div>
