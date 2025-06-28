ea9e16b7a9de62e4484fd51a82ef853d6419fae3
senpaiharde <xxslavan1@gmail.com>
2025-06-28 18:21:35 +0300

    styling the pricing landing

---
+++ ai-dcos/app/dashboard/upgrade/page.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+function PricingPage() {
+  return (
+    <div className='bg-white'>
+      <div className=''>
+        <div>
+          <h2 className="text-base font-semibold leading-7 text-indigo-600">
+            Pricing
+            </h2>
+          <p className="mt-2 text-4xl font-bold tracking-tight text-gray-900 sm:text-5xl">
+            Supercharge your Document Companion
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default PricingPage;
