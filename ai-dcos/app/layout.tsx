206625a22b7071ac6adb6f637bd9824ffe1c69a4
senpaiharde <xxslavan1@gmail.com>
2025-07-03 20:43:40 +0300

    fixed the color switching

--- ai-dcos/app/layout.tsx
+++ ai-dcos/app/layout.tsx
@@ -1,9 +1,7 @@
-import { ClerkProvider } from "@clerk/nextjs";
-import "./globals.css";
-import { Toaster } from "@/components/ui/sonner";
-import { ThemeProvider } from "@/components/theme-provider";
-
-
+import { ClerkProvider } from '@clerk/nextjs';
+import './globals.css';
+import { Toaster } from '@/components/ui/sonner';
+import { ThemeProvider } from '@/components/theme-provider';
 
 export default function RootLayout({
   children,
@@ -11,22 +9,19 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-     <ClerkProvider>
-         <html lang="en">
-      <body className="min-h-screen h-screen overflow-hidden flex flex-col"
-       
-      >
-         <Toaster />
-         <ThemeProvider
-          attribute="class" // adds `class="dark"` to html
-          defaultTheme="system" // follows OS theme by default
-          enableSystem
-          disableTransitionOnChange
-        ></ThemeProvider>
-        {children}
-      </body>
-    </html>
-     </ClerkProvider>
-   
+    <ClerkProvider>
+      <html lang="en">
+        <body className="min-h-screen h-screen overflow-hidden flex flex-col">
+          <Toaster />
+          <ThemeProvider
+            attribute="class" // adds `class="dark"` to html
+            defaultTheme="system" // follows OS theme by default
+            enableSystem
+            disableTransitionOnChange>
+            {children}
+          </ThemeProvider>
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
